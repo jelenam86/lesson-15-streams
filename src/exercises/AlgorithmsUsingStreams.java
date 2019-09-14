a823dadc4c6f019c75110644842d02044246efd4
jelenam86 <jelena.mat86@gmail.com>
2019-09-14 23:25:00 +0200

    Add algorithms

--- src/exercises/AlgorithmsUsingStreams.java
+++ src/exercises/AlgorithmsUsingStreams.java
@@ -1,7 +1,10 @@
 package exercises;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class AlgorithmsUsingStreams {
 
@@ -22,6 +25,85 @@ public class AlgorithmsUsingStreams {
 		 	.mapToObj(c -> Character.toString(c))	
 			.mapToInt(num -> Integer.parseInt(num))
 			.sum();
+	}
+
+	public static long countPearls(List<Boolean> oysters) {
+		return oysters
+				.stream()
+				.filter(pearl -> pearl == true)
+				.count();
+	}
+
+	public static String findLongestWord(List<String> words) {
+		return words
+				.stream()
+				.max(Comparator.comparingInt(String::length))
+				.get();
+	}
+
+	public static List<Double> sortScores(List<Double> results) {
+		return results
+				.stream()
+				.sorted()
+				.collect(Collectors.toList());
+	}
+
+	public static List<String> sortDNA(List<String> unsortedSequences) {
+		return unsortedSequences
+				.stream()
+				.sorted(Comparator.comparingInt(String::length))
+				.collect(Collectors.toList());
+	}
+
+	public static List<String> sortWords(List<String> words) {
+		return words
+				.stream()
+				.sorted()
+				.collect(Collectors.toList());
+	}
+
+	public static List<String> getDistinctSpellsInAlphabeticalOrder(List<String> spellsCastThisWeek) {
+		return spellsCastThisWeek
+				.stream()
+				.distinct()
+				.sorted()
+				.collect(Collectors.toList());
+	}
+
+	public static List<String> getBatPhrases(String[] batmanScript) {
+		return Arrays.stream(batmanScript)
+				.flatMap(Pattern.compile("\\s+")::splitAsStream)
+				.filter(x -> x.equalsIgnoreCase("bam") || x.equalsIgnoreCase("pow"))
+				.map(String::toUpperCase)
+				.collect(Collectors.toList());
+	}
+
+	public static long countXs(String xString) {
+		return xString
+				.chars()
+				.filter(x -> x == 'x' || x == 'X')
+				.count();
+	}
+
+	public static int addEven(String string) {
+		return string
+				.chars()
+				.mapToObj(c -> Character.toString(c))
+				.mapToInt(x -> Integer.parseInt(x))
+				.filter(x -> x % 2 == 0)
+				.sum();
+	}
+
+	public static double getAverage(int[] numbers) {
+		return Arrays.stream(numbers)
+				.average()
+				.getAsDouble();
+	}
+
+	public static long countOccurrences(String[] strArr, String string) {
+		return Arrays.stream(strArr)
+				.filter(x -> x.equalsIgnoreCase(string))
+				.count();
 	}
 
 }
